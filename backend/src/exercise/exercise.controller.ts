9ab54049d790b38c8ff43430b67410e2a7aa9c35
MaksZiem <mtziembicki@gmail.com>
2026-09-09 13:00:21 +0200

    exercise controller

--- backend/src/exercise/exercise.controller.ts
+++ backend/src/exercise/exercise.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { MuscleGroup } from 'src/enums/muscle-group.enum';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { CreateExerciseDto } from './dtos/create-exercise.dto';
+import { UpdateExerciseDto } from './dtos/update-exercise.dto';
 
 @Controller('exercise')
 export class ExerciseController {
@@ -11,5 +14,29 @@ export class ExerciseController {
   @UseGuards(AuthGuard)
   findAll(@Query('muscleGroup') muscleGroup?: MuscleGroup) {
     return this.exerciseService.findAll(muscleGroup);
+  }
+
+  @Get('/:id')
+  @UseGuards(AuthGuard)
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.exerciseService.findOne(id)
+  }
+
+  @Post()
+  @UseGuards(AdminGuard)
+  createExercise(@Body() dto: CreateExerciseDto) {
+    return this.exerciseService.create(dto)
+  }
+
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  updateExercise(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateExerciseDto) {
+    return this.exerciseService.update(id, dto)
+  }
+
+  @Delete('/:id')
+  @UseGuards(AdminGuard)
+  removeExercise(@Param('id', ParseIntPipe) id: number) {
+    return this.exerciseService.remove(id)
   }
 }
